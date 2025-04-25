0db8b5b92e5cb7570e432dbe663aa6834f998eee
taj-u-LW <tajcsebsmrstu@gmail.com>
2025-04-26 05:34:18 +0600

    create User views (API) and custom permissions for RBAC to  resources

--- users/views.py
+++ users/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions, status
+from rest_framework.response import Response
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework_simplejwt.views import TokenObtainPairView
+from django.conf import get_user_model
+from .serializers import UserSerializer, RegisterSerializer
+from .permissions import IsAdmin
 
-# Create your views here.
+User = get_user_model()
+
+class RegisterView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    permission_classes = [permissions.AllowAny]
+    serializer_class = RegisterSerializer
+
+class UserListView(generics.ListAPIView):
+    queryset = User.objects.all()
+    permission_classes = [IsAdmin]
+    serializer_class = UserSerializer
+    filterset_fields = ['role']
+    search_fields = ['username', 'email']
